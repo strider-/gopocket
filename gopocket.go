b3292bccb33cef8e9393451b827735bf8dc2f4f9
Michael Tighe <striderIIDX@gmail.com>
2013-05-20 14:48:32 -0500

    Initial Commit

---
+++ gopocket.go
@@ -0,0 +1,114 @@
+package gopocket
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"strconv"
+)
+
+const (
+	pocketAddUrl    = "https://getpocket.com/v3/add"
+	pocketModifyUrl = "https://getpocket.com/v3/send"
+)
+
+type Pocket struct {
+	key, token string
+}
+
+func Init(consumerKey, accessToken string) *Pocket {
+	return &Pocket{key: consumerKey, token: accessToken}
+}
+
+func (p *Pocket) Add(url, title string, tags []string) (*AddResponse, *ApiRate, error) {
+	data := struct {
+		ConsumerKey string   `json:"consumer_key"`
+		AccessToken string   `json:"access_token"`
+		Url         string   `json:"url"`
+		Title       string   `json:"title,omitempty"`
+		Tags        []string `json:"tags"`
+	}{
+		p.key, p.token, url, title, tags,
+	}
+
+	var resp *AddResponse
+	rate, err := p.post(pocketAddUrl, &data, &resp)
+	return resp, rate, err
+}
+
+func (p *Pocket) post(url string, requestModel interface{}, result interface{}) (rate *ApiRate, err error) {
+	// marshal the request struct to a json object within an io.Reader
+	body, err := marshalRequest(requestModel)
+	if err != nil {
+		return
+	}
+
+	// create a POST request to pocket
+	req, err := createRequest(url, body)
+	if err != nil {
+		return
+	}
+
+	// execute the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	// read & unmarshal the response into the expected result, along with the current api rate limits
+	defer resp.Body.Close()
+	rate = apiRateFromResponse(resp)
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(contents, &result)
+	return
+}
+
+func marshalRequest(request interface{}) (io.Reader, error) {
+	rawBody, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewReader(rawBody), nil
+}
+
+func createRequest(url string, body io.Reader) (req *http.Request, err error) {
+	req, err = http.NewRequest("POST", url, body)
+	if err != nil {
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("X-Accept", "application/json")
+	return
+}
+
+func apiRateFromResponse(response *http.Response) *ApiRate {
+	var rate = &ApiRate{}
+	t := reflect.ValueOf(rate).Elem()
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Type().Field(i)
+		value := t.Field(i)
+		tag := field.Tag.Get("header")
+		header := response.Header.Get(tag)
+
+		switch field.Type.Name() {
+		case "int":
+			num, _ := strconv.ParseInt(header, 10, 32)
+			value.SetInt(num)
+		case "string":
+			value.SetString(header)
+		case "int64":
+			num, _ := strconv.ParseInt(header, 10, 64)
+			value.SetInt(num)
+		}
+	}
+	return rate
+}
---
+++ response.go
@@ -0,0 +1,110 @@
+package gopocket
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+type ApiRate struct {
+	Error         string `header:"X-Error"`
+	ErrorCode     int    `header:"X-Error-Code"`
+	UserLimit     int    `header:"X-Limit-User-Limit"`
+	UserRemaining int    `header:"X-Limit-User-Remaining"`
+	UserReset     int64  `header:"X-Limit-User-Reset"`
+	KeyLimit      int    `header:"X-Limit-Key-Limit"`
+	KeyRemaining  int    `header:"X-Limit-Key-Remaining"`
+	KeyReset      int64  `header:"X-Limit-Key-Reset"`
+}
+
+type AddResponse struct {
+	Item   responseItem `json:"item"`
+	Status int          `json:"status"`
+}
+
+type ModifyResponse struct {
+	Results []responseItem `json:"action_results"`
+	Status  int            `json:"status"`
+}
+
+type responseItem struct {
+	ItemId            int       `json:"item_id,string"`
+	NormalUrl         string    `json:"normal_url"`
+	ResolvedNormalUrl string    `json:"resolved_normal_url"`
+	GivenUrl          string    `json:"given_url"`
+	ResolvedId        int       `json:"resolved_id,string"`
+	ResolvuedUrl      string    `json:"resolved_url"`
+	ExtendedItemId    int       `json:"extended_item_id,string"`
+	DomainId          int       `json:"domain_id,string"`
+	OriginDomainId    int       `json:"origin_domain_id,string"`
+	ResponseCode      int       `json:"response_code,string"`
+	MimeType          string    `json:"mime_type"`
+	ContentLength     int       `json:"content_length,string"`
+	Encoding          string    `json:"encoding"`
+	DateResolved      string    `json:"date_resolved"`
+	DatePublished     string    `json:"date_published"`
+	Title             string    `json:"title"`
+	Excerpt           string    `json:"excerpt"`
+	WordCount         int       `json:"word_count,string"`
+	LoginRequired     int       `json:"login_required,string"`
+	HasImage          int       `json:"has_image,string"`
+	HasVideo          int       `json:"has_video,string"`
+	IsIndex           int       `json:"is_index,string"`
+	IsArticle         int       `json:"is_article,string"`
+	UsedFallback      int       `json:"used_fallback,string"`
+	Authors           authorMap `json:"authors"`
+	Images            imageMap  `json:"images"`
+	Videos            videoMap  `json:"videos"`
+}
+
+type Author struct {
+	AuthorID int    `json:"author_id,string"`
+	Name     string `json:"name"`
+	Url      string `json:"url"`
+}
+
+type authorMap map[string]Author
+
+func (a *authorMap) UnmarshalJSON(b []byte) error {
+	if bytes.Compare([]byte("[]"), b) == 0 {
+		return nil
+	}
+	return json.Unmarshal(b, (*map[string]Author)(a))
+}
+
+type Image struct {
+	ItemId  int    `json:"item_id,string"`
+	ImageId int    `json:"image_id,string"`
+	Src     string `json:"src"`
+	Width   int    `json:"width,string"`
+	Height  int    `json:"height,string"`
+	Credit  string `json:"credit"`
+	Caption string `json:"caption"`
+}
+
+type imageMap map[string]Image
+
+func (i *imageMap) UnmarshalJSON(b []byte) error {
+	if bytes.Compare([]byte("[]"), b) == 0 {
+		return nil
+	}
+	return json.Unmarshal(b, (*map[string]Image)(i))
+}
+
+type Video struct {
+	ItemId  int    `json:"item_id,string"`
+	VideoId int    `json:"video_id,string"`
+	Src     string `json:"src"`
+	Width   int    `json:"width,string"`
+	Height  int    `json:"height,string"`
+	Type    int    `json:"type,string"`
+	Vid     string `json:"vid"`
+}
+
+type videoMap map[string]Video
+
+func (v *videoMap) UnmarshalJSON(b []byte) error {
+	if bytes.Compare([]byte("[]"), b) == 0 {
+		return nil
+	}
+	return json.Unmarshal(b, (*map[string]Video)(v))
+}
